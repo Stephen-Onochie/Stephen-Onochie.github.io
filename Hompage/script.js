5f1026b66f39a73d7ffd5d4d58a795f4f920b7f7
Stephen O <stephenconochie@gmail.com>
2023-06-02 18:34:49 -0400

    Revamped Files

--- Hompage/script.js
+++
@@ -1,7 +0,0 @@
-var defaultMessage = "Hey there! If you can see this, you must be a cool programmer! Or a secret hacker...";
-
-console.clear();
-
-console.log('-----------------------------------')
-console.log(defaultMessage);
-console.log('-----------------------------------')
