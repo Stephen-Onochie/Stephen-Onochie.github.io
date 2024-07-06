c8873052995d7fbf4b9bd0dacd935721568dbe86
Stephen Onochie <stephenconochie@gmail.com>
2024-07-06 14:18:01 -0400

    More Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,12 +37,9 @@ const About = () => {
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+        I am an aspiring software developer based in Avon, IN. 
+        For the past few years, I have built and cultivated skills in robotics, compuer vision, AI, and more.
+        
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
